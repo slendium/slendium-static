cd597e94b7ff3b5c29c4d444c600f8670211f13e
C. Fahner <cfahner.git.ixuk9phi@slendium.org>
2026-02-15 16:48:15 +0100

    Add Iteration utility + tests

---
+++ src/Common/Iteration.php
@@ -0,0 +1,195 @@
+<?php
+
+namespace Slendium\SlendiumStatic\Common;
+
+use Closure;
+use Stringable;
+
+/**
+ * @internal
+ * @author C. Fahner
+ * @copyright Slendium 2026
+ */
+final class Iteration {
+
+	private const ALWAYS_TRUE = static function() {
+		return true;
+	};
+
+	/**
+	 * @template TSourceKey
+	 * @template TSourceValue
+	 * @param iterable<TSourceKey,TSourceValue> $source
+	 * @param Closure(TSourceValue,TSourceKey):bool $predicate
+	 * @return ?TSourceValue
+	 */
+	public static function firstOrNull(iterable $source, Closure $predicate = self::ALWAYS_TRUE): mixed {
+		foreach ($source as $key => $value) {
+			if ($predicate($value, $key)) {
+				return $value;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * @template TSourceKey
+	 * @template TSourceValue
+	 * @param iterable<TSourceKey,TSourceValue> $source
+	 * @param Closure(TSourceValue,TSourceKey):bool $predicate
+	 */
+	public static function all(iterable $source, Closure $predicate): bool {
+		foreach ($source as $key => $value) {
+			if (!$predicate($value, $key)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * @template TSourceKey
+	 * @template TSourceValue
+	 * @param iterable<TSourceKey,TSourceValue> $source
+	 * @param Closure(TSourceValue,TSourceKey):bool $predicate
+	 */
+	public static function any(iterable $source, Closure $predicate = self::ALWAYS_TRUE): bool {
+		foreach ($source as $key => $value) {
+			if ($predicate($value, $key)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * @template TSourceKey
+	 * @template TSourceValue
+	 * @param iterable<TSourceKey,TSourceValue> $source
+	 * @param Closure(TSourceValue,TSourceKey):bool $predicate
+	 * @return iterable<TSourceKey,TSourceValue>
+	 */
+	public static function filter(iterable $source, Closure $predicate): iterable {
+		foreach ($source as $key => $value) {
+			if ($predicate($value, $key)) {
+				yield $key => $value;
+			}
+		}
+	}
+
+	/**
+	 * @template TSourceKey
+	 * @template TResult
+	 * @param iterable<TSourceKey,mixed> $source
+	 * @param class-string<TResult> $className
+	 * @return iterable<TSourceKey,TResult>
+	 */
+	public static function filterType(iterable $source, string $className): iterable {
+		foreach ($source as $key => $value) {
+			if (\is_object($value) && \is_a($value, $className)) {
+				yield $key => $value;
+			}
+		}
+	}
+
+	/**
+	 * @template TSourceKey
+	 * @template TSourceValue
+	 * @template TTargetValue
+	 * @param iterable<TSourceKey,TSourceValue> $source
+	 * @param Closure(TSourceValue,TSourceKey):TTargetValue $transform
+	 * @return iterable<TSourceKey,TTargetValue>
+	 */
+	public static function map(iterable $source, Closure $transform): iterable {
+		foreach ($source as $sourceKey => $sourceValue) {
+			yield $sourceKey => $transform($sourceValue, $sourceKey);
+		}
+	}
+
+	/**
+	 * @template TSourceKey
+	 * @template TSourceValue
+	 * @template TCarry
+	 * @param iterable<TSourceKey,TSourceValue> $source
+	 * @param Closure(TCarry,TSourceValue,TSourceKey):TCarry $reducer
+	 * @param TCarry $initial
+	 * @return TCarry
+	 */
+	public static function reduce(iterable $source, Closure $reducer, mixed $initial): mixed {
+		$carry = $initial;
+		foreach ($source as $key => $value) {
+			$carry = $reducer($carry, $value, $key);
+		}
+		return $carry;
+	}
+
+	/**
+	 * @template TSourceKey
+	 * @template TSourceValue
+	 * @template TResult
+	 * @param iterable<TSourceKey,TSourceValue> $source
+	 * @param Closure(TSourceValue,TSourceKey):iterable<TResult> $transform
+	 * @return iterable<TResult>
+	 */
+	public static function flatten(iterable $source, Closure $transform): iterable {
+		foreach ($source as $key => $value) {
+			yield from $transform($value, $key);
+		}
+	}
+
+	/**
+	 * Groups values of the iterable according to a key selector.
+	 *
+	 * This grouping uses the same rules as PHP's native array, where a numeric string key will be
+	 * cast to an int key.
+	 *
+	 * @template TSourceKey
+	 * @template TSourceValue
+	 * @template TGroupKey of string|int
+	 * @param iterable<TSourceKey,TSourceValue> $source
+	 * @param Closure(TSourceValue,TSourceKey):TGroupKey $getKey
+	 * @return iterable<TGroupKey,iterable<TSourceValue>>
+	 */
+	public static function group(iterable $source, Closure $getKey): iterable {
+		$grouped = [ ];
+		foreach ($source as $sourceKey => $value) {
+			$groupKey = $getKey($value, $sourceKey);
+			if (!isset($grouped[$groupKey])) {
+				$grouped[$groupKey] = [ ];
+			}
+			$grouped[$groupKey][] = $value;
+		}
+
+		yield from $grouped;
+	}
+
+	/**
+	 * @template TSource
+	 * @param iterable<TSource> $source
+	 * @return list<TSource>
+	 */
+	public static function toList(iterable $source): array {
+		$list = [ ];
+		foreach ($source as $value) {
+			$list[] = $value;
+		}
+		return $list;
+	}
+
+	/**
+	 * @template TSourceKey of array-key
+	 * @template TSourceValue
+	 * @param iterable<TSourceKey,TSourceValue> $source
+	 * @return array<TSourceKey,TSourceValue>
+	 */
+	public static function toArray(iterable $source): array {
+		$array = [ ];
+		foreach ($source as $key => $value) {
+			$array[$key] = $value;
+		}
+		return $array;
+	}
+
+	private function __construct() { }
+
+}
---
+++ tests/Common/IterationTest.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace Slendium\SlendiumStaticTests\Common;
+
+use Closure;
+use DateTime;
+
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
+
+use Slendium\SlendiumStatic\Common\Iteration;
+
+class IterationTest extends TestCase {
+
+	public static function firstOrNullCases(): iterable {
+		yield [ [ 0, 1, 2, 3 ], 0 ];
+		yield [ [ ], null ];
+	}
+
+	#[DataProvider('firstOrNullCases')]
+	public function test_firstOrNull_returnsExpectedItem(iterable $source, mixed $expectedResult): void {
+		$result = Iteration::firstOrNull($source);
+
+		$this->assertSame($expectedResult, $result);
+	}
+
+	public static function firstOrNullPredicateCases(): iterable {
+		yield [ [ 0, 1, 2, 3 ], fn($v) => $v === 1, 1 ];
+		yield [ [ 0, 1, 2, 3 ], fn($v) => $v === 5, null ];
+		yield [ [ ], fn($v) => $v === 2, null ];
+	}
+
+	#[DataProvider('firstOrNullPredicateCases')]
+	public function test_firstOrNull_returnsExpectedItem_whenPredicateGiven(iterable $source, Closure $predicate, mixed $expectedResult): void {
+		$result = Iteration::firstOrNull($source, $predicate);
+
+		$this->assertSame($expectedResult, $result);
+	}
+
+	public static function allCases(): iterable {
+		yield [ [ 0, 1, 2, 3, 4 ], fn($v) => \is_int($v), true ];
+		yield [ [ ], fn($v) => \is_int($v), true ];
+		yield [ [ 0, 1, '2', 3, 4 ], fn($v) => \is_int($v), false ];
+	}
+
+	#[DataProvider('allCases')]
+	public function test_all_returnsExpectedValue(iterable $source, Closure $predicate, bool $expectedResult): void {
+		$result = Iteration::all($source, $predicate);
+
+		$this->assertSame($expectedResult, $result);
+	}
+
+	public static function anyCases(): iterable {
+		yield [ [ 1, 2, 3 ], true ];
+		yield [ [ ], false ];
+	}
+
+	#[DataProvider('anyCases')]
+	public function test_any_returnsExpectedValue_whenPredicateNotGiven(iterable $source, bool $expectedResult): void {
+		$result = Iteration::any($source);
+
+		$this->assertSame($expectedResult, $result);
+	}
+
+	public static function anyPredicateCases(): iterable {
+		yield [ [ null, 0, 1, 2 ], fn($v) => \is_int($v), true ];
+		yield [ [ ], fn($v) => \is_int($v), false ];
+		yield [ [ 0, 1, 2, 3, 4 ], fn($v) => \is_string($v), false ];
+	}
+
+	#[DataProvider('anyPredicateCases')]
+	public function test_any_returnsExpectedValue_whenPredicateGiven(iterable $source, Closure $predicate, bool $expectedResult): void {
+		$result = Iteration::any($source, $predicate);
+
+		$this->assertSame($expectedResult, $result);
+	}
+
+	public function test_filter_returnsFilteredIterable(): void {
+		$values = [ '0', 0, 1, 2, 3, 'test', null, true, 4, (object)[] ];
+		$expectedResult = [ 0, 1, 2, 3, 4 ];
+
+		$result = Iteration::filter($values, fn($v) => \is_int($v))
+			|> Iteration::toList(...);
+
+		$this->assertSame($expectedResult, $result);
+	}
+
+	public function test_filterType_returnsFilteredIterable(): void {
+		$date = new DateTime;
+		$stdClass1 = (object)[ ];
+		$stdClass2 = (object)[ ];
+		$values = [ $date, $stdClass1, $stdClass2 ];
+		$expectedValues = [ $date ];
+
+		$result = Iteration::filterType($values, DateTime::class)
+			|> Iteration::toList(...);
+
+		$this->assertSame($expectedValues, $result);
+	}
+
+	public function test_map_returnsMappedIterable(): void {
+		$values = [ 0, 1, 2, 3 ];
+		$expectedResult = [ 0, 2, 4, 6 ];
+
+		$result = Iteration::map($values, fn($v) => $v*2)
+			|> Iteration::toList(...);
+
+		$this->assertSame($expectedResult, $result);
+	}
+
+	public function test_reduce_returnsReducedValue(): void {
+		$values = [ 2, 3, 4 ];
+		$expectedResult = 9;
+
+		$result = Iteration::reduce($values, fn($carry, $value) => $carry + $value, 0);
+
+		$this->assertSame($expectedResult, $result);
+	}
+
+	public function test_flatten_returnsFlattenedIterable(): void {
+		$values = [ 1, 2, 3 ];
+		$expectedResult = [ 1, 2, 2, 3, 3, 3 ];
+
+		$result = Iteration::flatten($values, fn($v) => \array_pad([ ], $v, $v))
+			|> Iteration::toList(...);
+
+		$this->assertSame($expectedResult, $result);
+	}
+
+	public function test_group_returnsGroupedIterable(): void {
+		$itemA1 = [ 'groupA', 1 ];
+		$itemA2 = [ 'groupA', 2 ];
+		$itemB1 = [ 'groupB', 3 ];
+		$values = [ $itemA1, $itemA2, $itemB1 ];
+		$expectedResult = [ 'groupA' => [ $itemA1, $itemA2 ], 'groupB' => [ $itemB1 ] ];
+
+		$result = Iteration::group($values, fn($v) => $v[0])
+			|> Iteration::toArray(...);
+
+		$this->assertSame($expectedResult, $result);
+	}
+
+}
