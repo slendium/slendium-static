35c941d6e8761d3c4f9a99b136fb4804e75297e2
C. Fahner <cfahner.git.ixuk9phi@slendium.org>
2026-02-18 06:50:27 +0100

    Add Resource base class

---
+++ src/Site/Resource.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Slendium\SlendiumStatic\Site;
+
+use Exception;
+
+use Slendium\SlendiumStatic\Source\File;
+
+/**
+ * @internal
+ * @author C. Fahner
+ * @copyright Slendium 2026
+ */
+abstract class Resource {
+
+	public static function fromFile(File $file): Exception|self {
+		return new Exception('Not implemented');
+	}
+
+	public Uri $uri {
+		get => $this->get_uri();
+	}
+
+	private function __construct(private readonly File $file) { }
+
+	public abstract function writeToFile(string $path): Exception|true;
+
+	private function get_uri(): Uri {
+		$current = $this->file->directory;
+		$path = [ $current->name ];
+		while ($current->ancestor !== null) {
+			$current = $current->ancestor;
+			$path[] = $current->name;
+		}
+		return new Uri([ ...\array_reverse($path), $this->file->normalizedName ]);
+	}
+
+}
