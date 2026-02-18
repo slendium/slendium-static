3b1037b826603cfdb01cbd217158c1eb47be244e
C. Fahner <cfahner.git.ixuk9phi@slendium.org>
2026-02-18 07:01:04 +0100

    Refactor DirectoryTest to uncomment some tests

--- tests/Source/DirectoryTest.php
+++ tests/Source/DirectoryTest.php
@@ -16,41 +16,41 @@ use Slendium\SlendiumStaticTests\Source\Mocks\MemoryFilesystem;
 
 class DirectoryTest extends TestCase {
 
-	//~ public function test_extractResources_shouldNeverQueryDotPaths(): void {
-		//~ $called = false;
-		//~ $pathFlagged = false;
-		//~ $callback = static function ($path) use (&$called, &$pathFlagged) {
-			//~ $called = true;
-			//~ $pathFlagged = $pathFlagged || $path === '.' || $path === '..';
-			//~ return false;
-		//~ };
-		//~ $sut = new Directory('', filesystem: new CallbackFilesystem(
-			//~ isDirectory: $callback,
-			//~ isFile: $callback,
-			//~ scanDirectory: static fn() => [ '.', '..', 'test' ],
-		//~ ));
+	public function test_extractResources_shouldNeverQueryDotPaths(): void {
+		$called = false;
+		$pathFlagged = false;
+		$callback = static function ($path) use (&$called, &$pathFlagged) {
+			$called = true;
+			$pathFlagged = $pathFlagged || $path === '.' || $path === '..';
+			return false;
+		};
+		$sut = new Directory('', filesystem: new CallbackFilesystem(
+			isDirectory: $callback,
+			isFile: $callback,
+			scanDirectory: static fn() => [ '.', '..', 'test' ],
+		));
 
-		//~ Iteration::toList($sut->extractResources());
+		Iteration::toList($sut->extractResources());
 
-		//~ $this->assertTrue($called);
-		//~ $this->assertFalse($pathFlagged);
-	//~ }
+		$this->assertTrue($called);
+		$this->assertFalse($pathFlagged);
+	}
 
-	//~ public static function sourceExceptionCases(): iterable {
-		//~ // ambiguous, both files should resolve to index.html
-		//~ yield [ new MemoryFilesystem([ 'tmp' => [ 'index.htm' => '', 'index.html' => '' ] ]) ];
-		//~ // page name cannot be empty
-		//~ yield [ new MemoryFilesystem([ 'tmp' => [ '.html' => '' ] ]) ];
-	//~ }
+	public static function sourceExceptionCases(): iterable {
+		// ambiguous, both files should resolve to index.html
+		yield [ new MemoryFilesystem([ 'tmp' => [ 'index.htm' => '', 'index.html' => '' ] ]) ];
+		// page name cannot be empty
+		yield [ new MemoryFilesystem([ 'tmp' => [ '.html' => '' ] ]) ];
+	}
 
-	//~ #[DataProvider('sourceExceptionCases')]
-	//~ public function test_extractResources_shouldYieldErrors_whenSourceInvalid(Filesystem $filesystem): void {
-		//~ $sut = new Directory('/tmp', filesystem: $filesystem);
+	#[DataProvider('sourceExceptionCases')]
+	public function test_extractResources_shouldYieldErrors_whenSourceInvalid(Filesystem $filesystem): void {
+		$sut = new Directory('/tmp', filesystem: $filesystem);
 
-		//~ $result = Iteration::toList($sut->extractResources());
+		$result = Iteration::toList($sut->extractResources());
 
-		//~ $this->assertTrue(Iteration::all($result, static fn($item) => $item instanceof SourceException));
-	//~ }
+		$this->assertTrue(Iteration::all($result, static fn($item) => $item instanceof SourceException));
+	}
 
 	public function test_extractResources_shouldYieldResources_whenSourceValid(): void {
 		$filesystem = new MemoryFilesystem([ 'tmp' => [
