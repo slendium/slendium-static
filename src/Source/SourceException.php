ff147058290038fd2d6bf420f1fe872a9791c075
C. Fahner <cfahner.git.ixuk9phi@slendium.org>
2026-02-18 06:47:31 +0100

    Add SourceException

---
+++ src/Source/SourceException.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Slendium\SlendiumStatic\Source;
+
+use Exception;
+
+/**
+ * @internal
+ * @author C. Fahner
+ * @copyright Slendium 2026
+ */
+class SourceException extends Exception {
+
+	/** @param non-empty-list<File> $files */
+	public static function forAmbiguousResource(array $files): self {
+		return new self(
+			"Ambiguous resource, `{$files[0]->normalizedName}` could be created from multiple source files: "
+				.\implode(', ', \array_map(fn($file) => $file->sourcePath, $files))
+		);
+	}
+
+	public static function forUnnamedResource(File $file): self {
+		return new self("Resource at `{$file->sourcePath}` should have a non-empty name");
+	}
+
+}
