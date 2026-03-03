95d0ab471b2f1085eeebf4b3c6d19d34fa89bc93
C. Fahner <cfahner.git.ixuk9phi@slendium.org>
2026-03-03 09:25:21 +0100

    Add Configs

---
+++ src/Configs.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Slendium\SlendiumStatic;
+
+use ArrayAccess;
+
+/**
+ * @since 1.0
+ * @phpstan-type ConfigsMap ArrayAccess<non-empty-string,mixed>|array<non-empty-string,mixed>
+ * @author C. Fahner
+ * @copyright Slendium 2026
+ */
+final class Configs {
+
+	private function __construct() { }
+
+}
