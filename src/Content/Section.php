1b571476b85f31624c41532613c46669eca0194c
C. Fahner <cfahner.git.ixuk9phi@slendium.org>
2026-03-04 15:02:07 +0100

    Add Section interface

---
+++ src/Content/Section.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Slendium\SlendiumStatic\Content;
+
+/**
+ * @since 1.0
+ * @author C. Fahner
+ * @copyright Slendium 2026
+ */
+interface Section {
+
+	/** @since 1.0 */
+	public function getHtml(): string;
+
+}
