6ee3c988f787c1eaef3cd7b7403e904ebeb50d6b
C. Fahner <cfahner.git.ixuk9phi@slendium.org>
2026-03-07 13:29:14 +0100

    Update RealFilesystem to implement ::copyFile() and fix ::writeFile() not working if the directory does not exist

--- src/Source/RealFilesystem.php
+++ src/Source/RealFilesystem.php
@@ -40,9 +40,24 @@ class RealFilesystem implements Filesystem {
 
 	#[Override]
 	public function writeFile(string $path, string $contents): Exception|true {
+		$this->ensureDirExists($path);
 		return \file_put_contents($path, $contents) === false
 			? new FilesystemException("An error occurred writing to file `$path`")
 			: true;
+	}
+
+	#[Override]
+	public function copyFile(string $sourcePath, string $targetPath): Exception|true {
+		$this->ensureDirExists($targetPath);
+		return \copy($sourcePath, $targetPath)
+			? true
+			: new FilesystemException("An error occurred copying `$sourcePath` to `$targetPath`");
+	}
+
+	private function ensureDirExists(string $path): void {
+		if (!\file_exists(\dirname($path))) {
+			\mkdir(\dirname($path), recursive: true);
+		}
 	}
 
 }
