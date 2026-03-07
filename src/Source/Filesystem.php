2d1a02ae8fcbfb6982f30774ad5406f35c7191fa
C. Fahner <cfahner.git.ixuk9phi@slendium.org>
2026-03-07 13:13:53 +0100

    Update Filesystem to add ::copyFile() and to mark it as public API for now

--- src/Source/Filesystem.php
+++ src/Source/Filesystem.php
@@ -7,21 +7,31 @@ use Exception;
 /**
  * Interface for mocking or virtualizing the filesystem.
  *
- * @internal
+ * @since 1.0
  * @author C. Fahner
  * @copyright Slendium 2026
  */
 interface Filesystem {
 
+	/** @since 1.0 */
 	public function isFile(string $path): bool;
 
+	/** @since 1.0 */
 	public function isDirectory(string $path): bool;
 
-	/** @return Exception|iterable<string> An error or a list of file/directory names */
+	/**
+	 * @since 1.0
+	 * @return Exception|iterable<string> An error or a list of file/directory names
+	 */
 	public function scanDirectory(string $path): Exception|iterable;
 
+	/** @since 1.0 */
 	public function readFile(string $path): Exception|string;
 
+	/** @since 1.0 */
 	public function writeFile(string $path, string $contents): Exception|true;
+
+	/** @since 1.0 */
+	public function copyFile(string $sourcePath, string $targetPath): Exception|true;
 
 }
