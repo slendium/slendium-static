c8e4b08de60e528529f3be70318cc1eead88f911
C. Fahner <cfahner.git.ixuk9phi@slendium.org>
2026-03-17 21:02:23 +0100

    Add Copyable interface

---
+++ src/Source/Copyable.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Slendium\SlendiumStatic\Source;
+
+use Exception;
+
+/**
+ * Interface for filesystem entities that support being copied.
+ *
+ * @since 1.0
+ * @author C. Fahner
+ * @copyright Slendium 2026
+ */
+interface Copyable {
+
+	/** @since 1.0 */
+	public function copyTo(Path $target): Exception|true;
+
+}
