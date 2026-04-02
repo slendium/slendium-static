00b2720081b75943c6e94ac80c704539b510e0cc
C. Fahner <cfahner.git.ixuk9phi@slendium.org>
2026-04-02 17:19:26 +0200

    Update FilesystemException to add some static factory methods for consistent error messages

--- src/Source/FilesystemException.php
+++ src/Source/FilesystemException.php
@@ -9,4 +9,26 @@ use Exception;
  * @author C. Fahner
  * @copyright Slendium 2026
  */
-final class FilesystemException extends Exception { }
+final class FilesystemException extends Exception {
+
+	/** @since 1.0 */
+	public static function forScanDirectoryError(string $path): Exception {
+		return new self("An error occurred while scanning directory `$path`");
+	}
+
+	/** @since 1.0 */
+	public static function forReadFileError(string $path): Exception {
+		return new self("An error occurred reading from file `$path`");
+	}
+
+	/** @since 1.0 */
+	public static function forWriteFileError(string $path): Exception {
+		return new self("An error occurred writing to file `$path`");
+	}
+
+	/** @since 1.0 */
+	public static function forCopyFileError(string $sourcePath, string $targetPath): Exception {
+		return new self("An error occurred copying `$sourcePath` to `$targetPath`");
+	}
+
+}
