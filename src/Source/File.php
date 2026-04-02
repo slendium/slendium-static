33fd0b5fd660fbf7853901c4812b40cc3b655237
C. Fahner <cfahner.git.ixuk9phi@slendium.org>
2026-04-02 18:28:41 +0200

    Update File to remove deprecated properties and make $extension public API

--- src/Source/File.php
+++ src/Source/File.php
@@ -3,10 +3,6 @@
 namespace Slendium\SlendiumStatic\Source;
 
 use Exception;
-use Override;
-
-use Slendium\SlendiumStatic\Source\Path;
-use Slendium\SlendiumStatic\Source\PathInfo;
 
 /**
  * @since 1.0
@@ -20,24 +16,9 @@ final class File {
 		get => $this->directory->path->append($this->name);
 	}
 
-	/** @internal @deprecated */
+	/** @since 1.0 */
 	public string $extension {
 		get => PathInfo::getExtension($this->path);
-	}
-
-	/** @internal @deprecated */
-	public string $normalizedName {
-		get => PathInfo::getNormalizedName($this->path);
-	}
-
-	/** @internal @deprecated */
-	public string $normalizedPath {
-		get => \dirname($this->path).'/'.$this->normalizedName;
-	}
-
-	/** @internal @deprecated */
-	public string $sourcePath {
-		get => \trim("{$this->directory->sourcePath}{$this->name}", '/');
 	}
 
 	/** @since 1.0 */
